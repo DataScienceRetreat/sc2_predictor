81f04ab76f9d80568c6246d9521d6b7910fd54e3
hellno <holgerniessner@me.com>
2016-11-23 14:51:46 +0100

    removed mistaken misc image in interestingness.csv

--- learn-regression.py
+++ learn-regression.py
@@ -1,4 +1,4 @@
-import os
+import os, sys
 #os.environ["THEANO_FLAGS"] = "mode=FAST_RUN,device=gpu,floatX=float32"
 os.environ["THEANO_FLAGS"] = "mode=FAST_RUN,device=cpu,floatX=float32"
 import numpy as np
@@ -14,18 +14,9 @@ from keras.optimizers import SGD
 
 from scipy.misc import imread, imresize
 
-from VideoHelper.HelperFunctions import get_files_in_dir
+from VideoHelper.HelperFunctions import get_files_in_dir, path_is_dir
 
-img_width = 160
-img_height = 90
-
-regression_range = list(range(0, 5))
-
-img_path = '/Users/holger/dev/projects/sc2_predictor/data/img/'
-csv_path = '/Users/holger/dev/projects/sc2_predictor/img_class_full.csv'
-
-
-def csv_to_data(csv_path, target_shape):
+def csv_to_data(csv_path, img_path, target_shape):
     df = pd.read_csv(csv_path)
     X = np.array([imresize(imread(img_path + row['filename'] + '.png'),
                            size=target_shape).transpose(2, 0, 1) for index, row in df.iterrows()], dtype=np.float32)
@@ -33,41 +24,60 @@ def csv_to_data(csv_path, target_shape):
     y = df.iloc[:, 1]
     return (X, y)
 
-print('loading data from csv')
+def main(args):
+    img_width = 160
+    img_height = 90
 
-X, y = csv_to_data(csv_path, (img_width, img_height))
-X_train, y_train, X_test, y_test = train_test_split(X, y, test_size=0.3)
+    regression_range = list(range(0, 5))
 
-nb_epoch = 15
-batch_size = 32
+    if(len(args) != 3):
+        print('give image folder and csv regression file pls')
+        return -1
 
-print('building neural network')
+    img_path = args[1]
+    if(not path_is_dir(img_path)):
+        return -1
 
-model = Sequential()
-model.add(Convolution2D(32, 3, 3, border_mode='same', input_shape=(3, img_width, img_height)))
-model.add(Activation('relu'))
-model.add(Convolution2D(32, 3, 3, border_mode='same',  input_shape=(3, img_width, img_height)))
-model.add(Activation('relu'))
-model.add(MaxPooling2D(pool_size=(2, 2)))
-model.add(Dropout(0.5))
+    csv_path = args[2]
 
-model.add(Flatten())
-model.add(Dense(512))
-model.add(Activation('relu'))
-model.add(Dropout(0.5))
-model.add(Dense(1))
-model.add(Activation('linear'))
-model.compile(loss='mean_squared_error', optimizer='rmsprop')
+    print('loading data from csv')
 
-# read csv file with filename and interestingness value
-# merge files into huge numpy array
-# split into train and test
-# fit and evaluate model
+    X, y = csv_to_data(csv_path, img_path, (img_width, img_height))
+    X_train, y_train, X_test, y_test = train_test_split(X, y, test_size=0.3)
 
-print('fitting model')
+    nb_epoch = 15
+    batch_size = 32
 
-model.fit(X_train, y_train, nb_epoch=nb_epoch, batch_size=batch_size)
-#score = model.evaluate(X_test, y_test, batch_size=batch_size)
-y_pred = model.predict(X_test)
-mse = mean_squared_error(y_test, y_pred)
-print('mean squared error {}'.format(mse))
+    print('building neural network')
+
+    model = Sequential()
+    model.add(Convolution2D(32, 3, 3, border_mode='same', input_shape=(3, img_width, img_height)))
+    model.add(Activation('relu'))
+    model.add(Convolution2D(32, 3, 3, border_mode='same',  input_shape=(3, img_width, img_height)))
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Dropout(0.5))
+
+    model.add(Flatten())
+    model.add(Dense(512))
+    model.add(Activation('relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(1))
+    model.add(Activation('linear'))
+    model.compile(loss='mean_squared_error', optimizer='rmsprop')
+
+    # read csv file with filename and interestingness value
+    # merge files into huge numpy array
+    # split into train and test
+    # fit and evaluate model
+
+    print('fitting model')
+
+    model.fit(X_train, y_train, nb_epoch=nb_epoch, batch_size=batch_size)
+    #score = model.evaluate(X_test, y_test, batch_size=batch_size)
+    y_pred = model.predict(X_test)
+    mse = mean_squared_error(y_test, y_pred)
+    print('mean squared error {}'.format(mse))
+
+if __name__ == "__main__":
+    main(sys.argv)
